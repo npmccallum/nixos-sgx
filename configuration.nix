3a557d2d734b797da90ca79d8dc4a4f9a49833b9
Nathaniel McCallum <npmccallum@redhat.com>
2019-11-20 18:34:15 -0500

    First stab at building an SGX kernel

--- configuration.nix
+++ configuration.nix
@@ -46,5 +46,30 @@
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIoZP5bZedmrj/lidLkKXhvZwwl9Pj5VxLV22nXhkijt7UJhSUX/rOV4Kg/wmR5ptMjGyE4PPSHmCEzXvQnpyMU= nathaniel@mccallum.life"
     ];
   };
+
+  boot.kernelPackages = let
+      linux_sgx_pkg = { stdenv, fetchurl, buildLinux, ... } @ args:
+
+        with stdenv.lib;
+
+        buildLinux (args // rec {
+          version = "5.4.0-rc3";
+          modDirVersion = "5.4.0-rc3";
+
+          src = fetchurl {
+            url = "https://github.com/jsakkine-intel/linux-sgx/archive/v23.tar.gz";
+            sha256 = "11rwlwv7s071ia889dk1dgrxprxiwgi7djhg47vi56dj81jgib20";
+          };
+          kernelPatches = [];
+
+          extraConfig = ''
+            INTEL_SGX y
+          '';
+
+          extraMeta.branch = "5.4";
+        } // (args.argsOverride or {}));
+      linux_sgx = pkgs.callPackage linux_sgx_pkg{};
+    in
+      pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_sgx);
 }
 
