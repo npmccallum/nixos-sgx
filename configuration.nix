047d2a42f04531e2cdd38747c0d7efa3fa816ef1
Nathaniel McCallum <npmccallum@redhat.com>
2019-11-23 14:05:53 -0500

    Enable Red Hat DDNS

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,24 @@
   networking.wireless.enable = false;
   networking.interfaces.eno1.useDHCP = true;
 
+  systemd.timers.ddns = {
+    enable = true;
+    timerConfig.OnCalendar = "*:0/5";
+    wantedBy = [ "timer.target" "multi-user.target" ];
+  };
+
+  systemd.services.ddns = {
+    serviceConfig.Type = "oneshot";
+    path = with pkgs; [ curl ];
+    script = ''
+      while read name domain hash; do
+	url="http://ddns.corp.redhat.com/redhat-ddns/updater.php?name=$name&domain=$domain&hash=$hash"
+	rep=$(curl -s "$url")
+	! echo $rep | grep ERROR
+      done < /etc/ddns.conf
+    '';
+  };
+
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
